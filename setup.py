f08107e27a95dd7d4f0d64b743722794d372061f
Saad Ibrahim <hope12forever10@gamil.com>
2025-04-08 15:21:02 +0300

    third commit

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 name='mlproject',
 version='0.0.1',
 author='Saad',
-author_email='hope12f@gmail.com',
+author_email='hope12forever@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
